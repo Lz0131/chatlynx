cfb574ec1784aa6802a4c874ab324b957324191b
Lz0131 <145055772+Lz0131@users.noreply.github.com>
2024-05-01 14:56:55 -0600

    primeros archivos de firebase
    solo se agregaron las credenciales

--- lib/features/call/screens/call_screen.dart
+++ lib/features/call/screens/call_screen.dart
@@ -1,10 +1,10 @@
 import 'package:agora_uikit/agora_uikit.dart';
-import 'package:chatlynx/common/widgets/loader.dart';
-import 'package:chatlynx/config/agora_config.dart';
-import 'package:chatlynx/features/call/controller/call_controller.dart';
 import 'package:chatlynx/models/call.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:chatlynx/common/widgets/loader.dart';
+import 'package:chatlynx/config/agora_config.dart';
+import 'package:chatlynx/features/call/controller/call_controller.dart';
 
 class CallScreen extends ConsumerStatefulWidget {
   final String channelId;
